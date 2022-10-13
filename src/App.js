919417e1674c7cb242b0a2a5c5f65f65ee2a7b14
saiffaw <saif_har@hotmail.com>
2022-10-13 10:56:41 +0100

    react checkpoint

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BasicExample from "./componets/BasicExample";
+import Navbaree from "./componets/Navbaree";
+
+function App() {
+  return (
+    <>
+      <Navbaree />
+      <BasicExample />
+    </>
+  );
+}
+
+export default App;
